44337615913529fdc0e74f373909607a7ca84508
TerrificTable <omegagaming189@gmail.com>
2023-02-10 22:29:32 +0100

    a

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 
-	ws := webserver.NewWebServer("OTE2MDkxMjU4MDEzODkyNjk5.GC2FP8.nMG5t7l2WnUBmSNNQBEhQgDEsYEbF-ANHRGq7U")
+	ws := webserver.NewWebServer("")
 	if err := ws.Start(2023); err != nil {
 		panic(err)
 	}
